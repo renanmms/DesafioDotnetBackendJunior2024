09af68db2058c900e93bb4da3a1e862c9b31d42e
Renan Martins <renanmartins1999@hotmail.com>
2024-02-26 07:59:54 -0300

    [FEAT] Contact entity

---
+++ TesteBackendEnContact/Core/Domain/ContactBook/Contact.cs
@@ -0,0 +1,14 @@
+namespace TesteBackendEnContact.Core.Domain.ContactBook
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public int ContactBookId { get; set; }
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public ContactBook ContactBook { get; set; }
+    }
+}
